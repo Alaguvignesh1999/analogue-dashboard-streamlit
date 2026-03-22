9ef0dc3a0a90b378da3a4cb32cab59e9d83fb8c0
Alaguvignesh1999 <144868535+Alaguvignesh1999@users.noreply.github.com>
2026-03-22 21:39:59 +0800

    chore: upgrade next runtime and clear advisories

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  outputFileTracingRoot: path.resolve(__dirname),
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
